8388e2b377efaa584f3a081532d9a79a5606fa75
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-10 21:43:59 -0700

    Add files via upload
    a program to create(initialize) a deck of playing cards, then shuffle the cards at random, and output 4 cards using vectors

--- main.cpp
+++ main.cpp
@@ -1,76 +1,45 @@
 //
 //  main.cpp
-//  computeDeviation7_11
+//  deckOfCardsUsingVectors
 //
-//  Created by Gregory Francois on 3/1/19.
+//  Created by Gregory Francois on 3/22/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
-#include <cmath>
-
-using namespace std;
-
-// Compute the mean of an array of double values
-double mean(const double x[] , int size , double y){
-    
-    // Declare value to store mean
-    double meanResult = y / size ;
-    
-    // Return mean when function is called
-    return meanResult;
-};
-// Compute the deviation of double values
-double deviation(const double x[] , int size , double y){
-    
-    // Declare variable to hold summation of discriminant numerator
-    double total = 0.0;
-    // Loop to sum each element subtracted by the mean, then square each iteration
-    // and hold that result in total variable
-    for (int i = 0; i < size; i++) {
-        total += pow(x[i] - y, 2.0) ;
-    }
-    // Declare variable for discriminant denominator
-    double discrDenom = size - 1.0;
-    // Declare variable to compute value of discriminant
-    double discr = total / discrDenom ;
-    // Declare variable to compute standard deviation
-    double deviationResult = sqrt(discr);
-    
-    // Return standard deviation when function is called
-    return deviationResult;
-};
-
+#include <vector>
+#include <string>
+#include <ctime>
 
 int main()
 {
+    using std::cout;
+    using std::vector;
+    using std::string;
     
+    const int NUMBER_OF_CARDS = 52;
+    vector<int> deck(NUMBER_OF_CARDS);  // Create vector deck
     
-    // Declare array
-    const int NUMBER_OF_ELEMENTS = 10;
-    double numbers[NUMBER_OF_ELEMENTS];
-    // Declare variable to hold sum to be divided by the size
-    // To get the mean or average
-    double sum = 0;
+    string suits[4] = {"Spades","Hearts","Diamonds","Clubs"};
+    string ranks[13] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
     
-    // Prompt user to enter ten numbers
-    cout << "Enter ten numbers: " << endl;
-    
-    // Loop to walk the array, summing the elements
-    // And storing the value in sum variable
-    for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
-        cin >> numbers[i];
-        sum += numbers[i];
+    // Initialize cards
+    for (int i = 0; i < NUMBER_OF_CARDS; i++)
+        deck[i] = i;
+    // Shuffle the cards
+    srand((int)time(0));
+    for (int i = 0; i < NUMBER_OF_CARDS; i++)
+    {
+        // Generating an index randomly
+        int index = rand() % NUMBER_OF_CARDS;
+        int temp = deck[i];
+        deck[i] = deck[index];
+        deck[index] = temp;
     }
-    // Created variable to store index of a number in array variable y;
-    // Created variable to store mean of array to pass to function
-    double arrayMean = sum / NUMBER_OF_ELEMENTS;
-    
-    // Call functions to output mean and SD to user
-    cout << "The mean is " << mean(numbers, NUMBER_OF_ELEMENTS, sum) << endl;
-    cout << "The standard deviation is " << deviation(numbers, NUMBER_OF_ELEMENTS, arrayMean) << endl;
-    
-    
-    
+    // Display the first four cards
+    for (int i = 0; i < 4; i++)
+    {
+        cout << ranks[deck[i] % 13] << " of " << suits[deck[i] / 13] << "\n";
+    }
     return 0;
 }
