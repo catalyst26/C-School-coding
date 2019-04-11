3542f05051ce78bc008d26e2f6fcb4417a7f1632
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-10 21:37:51 -0700

    Add files via upload
    Here I prompt user to enter numbers into 2 separate matrices, the program stores the user's input, then adds them together, and outputs the result back to the user

--- main.cpp
+++ main.cpp
@@ -1,56 +1,71 @@
 //
 //  main.cpp
-//  midterm question 4
+//  addTwoMatrices8_5
 //
-//  Created by Gregory Francois on 2/19/19.
+//  Created by Gregory Francois on 3/1/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
-#include <cctype>
-#include <string>
+#include <iomanip>
 using namespace std;
 
+// Declare array size and function
+const int N = 3;
+void addMatrix(const double a[][N], const double b[][N], double c[][N]){
+    
+    // For loop to print out the individual elements in the array
+    // After summing the two matrices
+    for (int row = 0; row < 3; row++) {
+        for (int column = 0; column < 3; column++) {
+            // Output result when function is called
+            cout << c[row][column] << " "; }
+        cout << endl;
+    }
+};
 
-int main()
-{
+
+int main() {
     
-    // Initialize variable to capture user sentence input
-    string userStr = "abcdefg";
-    // Prompt user to enter a sentence
-    cout << "Enter a sentence (Mary had a little lamb): " << endl;
-    // Getline function to capture the user's entire message
-    // Not just the first character
-    getline(cin, userStr);
-    // Print out the sentence back to the user
-    cout << "Your sentence is: " << userStr << endl;
-    cout << "Your sentence has " << userStr.size() << " characters in it." << endl;
+    // Declare and initialize 2 dimensional array
+    const int ROW_SIZE = 3;
+    const int COLUMN_SIZE = 3;
+    double matrixOne[ROW_SIZE][COLUMN_SIZE];
+    double matrixTwo[ROW_SIZE][COLUMN_SIZE];
+    // Prompt user to enter first matrix
+    cout << "Enter matrix 1: " << endl;
+    // For loop to fill up the rows and colums of the array
+    // After each iteration
+    for (int i = 0; i < ROW_SIZE; i++)
+        for (int j = 0; j < COLUMN_SIZE; j++){
+            // Store user input in array
+            cin >> matrixOne[i][j];
+        }
+    // Prompt user to enter second matrix
+    cout << "Enter matrix 2: " << endl;
+    for (int i = 0; i < ROW_SIZE; i++)
+        for (int j = 0; j < COLUMN_SIZE; j++){
+            cin >> matrixTwo[i][j];
+        }
     
-    // For loop to iterate through string for the length
-    // Updating the word count after each whitespace, incrementing by 1
-    int wordCount = 1;
-    for (int i = 0; i < userStr.length(); i++) {
-        if (userStr.at(i) == ' ')
-        {
-            wordCount++;
+    // Declare and initialize array to hold sum of
+    // The first and second arrays
+    const int X_SIZE = 3;
+    const int Y_SIZE = 3;
+    double total[X_SIZE][Y_SIZE];
+    // For loop to add each element in both arrays
+    // after each iteration and store the total
+    // in an array called total
+    for (int i = 0; i < X_SIZE; i++) {
+        for (int j = 0; j < Y_SIZE; j++) {
+            total[i][j] = matrixOne[i][j] + matrixTwo[i][j];
         }
     }
-    // Outputting the word count
-    cout << "The number of words is " << wordCount << endl;
-    // Using the subscript operator with size function to return second to last character
-    cout << "The second to last character using subscript operator is " << userStr[userStr.size()-2] << endl;
-    // Using the at function with size function to return second to last character
-    cout << "The second to last character using at function is " << userStr.at(userStr.size()-2) << endl;
+    
+    // Call function to output sum of matrices to the user
+    cout << "The addition of the matrices is " << endl;
+    addMatrix(matrixOne, matrixTwo, total);
     
     
     return 0;
 }
-
-// Enter a sentence (Mary had a little lamb):
-// Let's go to the movies
-// Your sentence is: Let's go to the movies
-// Your sentence has 22 characters in it.
-// The number of words is 5
-// The second to last character using subscript operator is e
-// The second to last character using at function is e
-// Program ended with exit code: 0
