2dc714f8880c82f166d8aad7de0bbaa696c68320
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-10 21:16:46 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,72 @@
 //
 //  main.cpp
-//  convert_Temp
+//  midterm question 1
 //
-//  Created by Gregory Francois on 3/22/19.
+//  Created by Gregory Francois on 2/19/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
-// function that takes the address of variable of type double
-// and applies Centigrade-to-fahrenheit conversion
-void convertTemp(double* c)
-{
-    using std::cout;
-    
-    *c = ((*c * 1.8) + 32);
-    cout << "The degrees in fahrenheit is " << *c << "\n";
-};
+
+// Inputs : 3 2 4 for a valid answer
+// Inputs: 1 2 4 for a negative discriminant
+// Inputs: 3 4 4 for denominator == 0
 
 int main()
 {
-    using std::cout;
-    using std::cin;
     
-    double celsius = 0;
-    cout << "Enter the degrees in celsius: \n";
-    cin >> celsius;
+    // Declare variables
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
     
-    convertTemp(&celsius);
-    cout << "After conversion the value contained in the variable celsius is " << celsius << "\n";
+    // Prompt user to enter variables
+    cout << "Enter three variables seperated by spaces (x y z): " << endl;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    
+    // Store result of sqrt function, denominator & equation
+    // in variables
+    double discr = ( 3 * x - z );
+    double squareRt = sqrt( discr );
+    double denominator = ( y - z ) ;
+    double equation = ( squareRt + 2 * y * y) / denominator ;
+    
+    // Checks for positive discriminant and non-undefined denominator
+    if (discr < 0)
+        cout << "Discriminant is negative. Result cannot be evaluated." << endl;
+    
+    else if (denominator == 0)
+        cout << "Denominator equal to 0. Result cannot be evaluated." << endl;
+    
+    // Print out equation result if the checks are passed
+    else
+        cout << "Answer is: " << setprecision(3) << equation << "." << endl;
+    
+
     
     return 0;
 }
+
+// Enter three variables seperated by spaces (x y z):
+//3 4 4
+//Denominator equal to 0. Result cannot be evaluated.
+//Program ended with exit code: 0
+
+//Enter three variables seperated by spaces (x y z):
+//1 2 4
+//Discriminant is negative. Result cannot be evaluated.
+//Program ended with exit code: 0
+
+//Enter three variables seperated by spaces (x y z):
+//3 2 4
+//Answer is: -5.12.
+//Program ended with exit code: 0
+
+
+
