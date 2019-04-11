c738326a151d5d01ee7abe8bebc9322caf1a3b23
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-10 21:26:59 -0700

    Add files via upload
    here I generate two random numbers between 10 and 100 and check whether the two individual digits are the same or no

--- main.cpp
+++ main.cpp
@@ -1,72 +1,40 @@
 //
 //  main.cpp
-//  midterm question 1
+//  midterm question 2
 //
 //  Created by Gregory Francois on 2/19/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
-#include <iomanip>
-#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <string>
 using namespace std;
-
-
-// Inputs : 3 2 4 for a valid answer
-// Inputs: 1 2 4 for a negative discriminant
-// Inputs: 3 4 4 for denominator == 0
 
 int main()
 {
+    // Initialize the random seed generator
+    // Cast the random generator as an unsigned int to remove compiler warning
+    srand((unsigned int)time(0));
     
-    // Declare variables
-    double x = 0.0;
-    double y = 0.0;
-    double z = 0.0;
+    // Generates a two-digit random number
+    // Assign random number to variable
+    int randNumOne = 10 + rand() % 90;
     
-    // Prompt user to enter variables
-    cout << "Enter three variables seperated by spaces (x y z): " << endl;
-    cin >> x;
-    cin >> y;
-    cin >> z;
+    // Convert random number integer variable to string variable
+    string randStr = to_string(randNumOne);
     
-    // Store result of sqrt function, denominator & equation
-    // in variables
-    double discr = ( 3 * x - z );
-    double squareRt = sqrt( discr );
-    double denominator = ( y - z ) ;
-    double equation = ( squareRt + 2 * y * y) / denominator ;
-    
-    // Checks for positive discriminant and non-undefined denominator
-    if (discr < 0)
-        cout << "Discriminant is negative. Result cannot be evaluated." << endl;
-    
-    else if (denominator == 0)
-        cout << "Denominator equal to 0. Result cannot be evaluated." << endl;
-    
-    // Print out equation result if the checks are passed
+    // Check if individual digits are the same or not
+    // print out respective statements
+    if ( randStr.at(0) == randStr.at(1) )
+        cout << "The random number is: " << randStr << ". The two individual digits are the same."<< endl;
     else
-        cout << "Answer is: " << setprecision(3) << equation << "." << endl;
+        cout << "The random number is: " << randStr << ". The two individual digits are not the same." << endl;
     
-
     
     return 0;
 }
 
-// Enter three variables seperated by spaces (x y z):
-//3 4 4
-//Denominator equal to 0. Result cannot be evaluated.
-//Program ended with exit code: 0
-
-//Enter three variables seperated by spaces (x y z):
-//1 2 4
-//Discriminant is negative. Result cannot be evaluated.
-//Program ended with exit code: 0
-
-//Enter three variables seperated by spaces (x y z):
-//3 2 4
-//Answer is: -5.12.
-//Program ended with exit code: 0
-
-
-
+// The random number is: 66. The two individual digits are the same. Program ended with exit code: 0
+// The random number is: 27. The two individual digits are not the same. Program ended with exit code: 0
