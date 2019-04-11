6c8d862c57d774ffb7b0641f768c06d76b6df9e3
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-10 21:34:48 -0700

    Add files via upload
    Here I prompt the user to enter a sentence, the program then outputs how many characters & words there are in their entered sentence. The program also spits out what the second to last character is.

--- main.cpp
+++ main.cpp
@@ -1,56 +1,56 @@
 //
 //  main.cpp
-//  midterm question 3
+//  midterm question 4
 //
 //  Created by Gregory Francois on 2/19/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
+#include <cctype>
+#include <string>
 using namespace std;
+
 
 int main()
 {
     
-    // Initialize character variable for user input
-    char userCharacter = '0';
-    // Prompt user to enter a single character
-    cout << "Enter a single character (4, f, B, etc.): " << endl;
-    // Store user input in the initialized variable
-    cin >> userCharacter;
+    // Initialize variable to capture user sentence input
+    string userStr = "abcdefg";
+    // Prompt user to enter a sentence
+    cout << "Enter a sentence (Mary had a little lamb): " << endl;
+    // Getline function to capture the user's entire message
+    // Not just the first character
+    getline(cin, userStr);
+    // Print out the sentence back to the user
+    cout << "Your sentence is: " << userStr << endl;
+    cout << "Your sentence has " << userStr.size() << " characters in it." << endl;
     
-    // If-Else statement to return whether the entered character
-    // is uppercase, lowercase, an integer or some other character
+    // For loop to iterate through string for the length
+    // Updating the word count after each whitespace, incrementing by 1
+    int wordCount = 1;
+    for (int i = 0; i < userStr.length(); i++) {
+        if (userStr.at(i) == ' ')
+        {
+            wordCount++;
+        }
+    }
+    // Outputting the word count
+    cout << "The number of words is " << wordCount << endl;
+    // Using the subscript operator with size function to return second to last character
+    cout << "The second to last character using subscript operator is " << userStr[userStr.size()-2] << endl;
+    // Using the at function with size function to return second to last character
+    cout << "The second to last character using at function is " << userStr.at(userStr.size()-2) << endl;
     
-    if (isalnum(userCharacter) && isdigit(userCharacter))
-        cout << "You entered " << userCharacter << ". It is a numerical character." << endl;
-    else if (isalnum(userCharacter) && isupper(userCharacter))
-        cout << "You entered " << userCharacter << ". It is a uppercase aplhabetical character." << endl;
-    else if (isalnum(userCharacter) && islower(userCharacter))
-        cout << "You entered " << userCharacter << ". It is a lowercase aplhabetical character." << endl;
-    else
-        cout << "You entered " << userCharacter << ". It is some other character" << endl;
     
     return 0;
 }
 
-//Enter a single character (4, f, B, etc.):
-//P
-//You entered P. It is a uppercase aplhabetical character.
-
-//Enter a single character (4, f, B, etc.):
-//u
-//You entered u. It is a lowercase aplhabetical character.
-//Program ended with exit code: 0
-
-//Enter a single character (4, f, B, etc.):
-//5
-//You entered 5. It is a numerical character.
-//Program ended with exit code: 0
-
-//Enter a single character (4, f, B, etc.):
-//]
-//You entered ]. It is some other character
-//Program ended with exit code: 0
-
-
+// Enter a sentence (Mary had a little lamb):
+// Let's go to the movies
+// Your sentence is: Let's go to the movies
+// Your sentence has 22 characters in it.
+// The number of words is 5
+// The second to last character using subscript operator is e
+// The second to last character using at function is e
+// Program ended with exit code: 0
